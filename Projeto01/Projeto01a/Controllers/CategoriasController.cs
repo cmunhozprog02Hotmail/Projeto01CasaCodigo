a525996bec1b02f8bea9f097bfc549497ea53b8c
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-03-31 17:03:04 -0300

    capitulo 1.4

--- Projeto01/Projeto01a/Controllers/CategoriasController.cs
+++ Projeto01/Projeto01a/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projeto01a.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,34 @@ namespace Projeto01a.Controllers
 {
     public class CategoriasController : Controller
     {
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria() {
+                CategoriaId = 1,
+                Nome = "Notebooks"
+                },
+                new Categoria() {
+                CategoriaId = 2,
+                Nome = "Monitores"
+                },
+                new Categoria() {
+                CategoriaId = 3,
+                Nome = "Impressoras"
+                },
+                new Categoria() {
+                CategoriaId = 4,
+                Nome = "Mouses"
+                },
+                new Categoria() {
+                CategoriaId = 5,
+                Nome = "Desktops"
+                }
+         };
+    
         // GET: Categorias
         public ActionResult Index()
         {
-            return View();
+            return View(categorias);
         }
     }
 }
