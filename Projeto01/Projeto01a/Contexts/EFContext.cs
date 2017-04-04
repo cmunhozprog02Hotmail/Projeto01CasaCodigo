c6a897f6e22c02c4127b289fea1ce17c4188da6b
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-04-03 23:39:46 -0300

    Pág. 48 - Inicial EF Fabricante

---
+++ Projeto01/Projeto01a/Contexts/EFContext.cs
@@ -0,0 +1,13 @@
+﻿using Projeto01a.Models;
+using System.Data.Entity;
+
+
+namespace Projeto01a.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
---
+++ Projeto01/Projeto01a/Controllers/FabricantesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Projeto01a.Contexts;
+using Projeto01a.Models;
+
+namespace Projeto01a.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
