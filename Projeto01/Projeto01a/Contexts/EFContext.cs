f34d91a4132322f5e7bb770b3dad00e2105cd56d
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-04-16 13:16:40 -0300

    Pág. 106 - Visão Index Produtos

--- Projeto01/Projeto01a/Contexts/EFContext.cs
+++ Projeto01/Projeto01a/Contexts/EFContext.cs
@@ -6,7 +6,9 @@ namespace Projeto01a.Contexts
 {
     public class EFContext : DbContext
     {
-        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public EFContext() : base("Asp_Net_MVC_CS") {
+            Database.SetInitializer<EFContext>(new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
         public DbSet<Produto> Produtos { get; set; }
--- Projeto01/Projeto01a/Models/Produto.cs
+++ Projeto01/Projeto01a/Models/Produto.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+
 namespace Projeto01a.Models
 {
     public class Produto
